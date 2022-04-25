665aa0729ceab69fc4c8fae3610fe30c4e2cbd0c
AndreSantiago08 <harper_hska_5@hotmail.com>
2022-04-25 00:51:03 -0500

    Pasando requerimiento 2 sprint 2

--- twitter/app/services/UserService.js
+++ twitter/app/services/UserService.js
@@ -4,6 +4,13 @@ class UserService{
     static create(id, userName, name){
         return new User(id, userName, name, "Sin bio")
     }
+
+    static getInfo(User){
+        const lista = [User.id, User.userName, User.name, "Sin bio"];
+        return lista;
+    }
 }
+
+
 
 module.exports = UserService;
--- twitter/test/services/UserService.test.js
+++ twitter/test/services/UserService.test.js
@@ -9,4 +9,14 @@ describe('Test for UserService', () => {
         expect(User.id).toBe(1);
         expect(User.bio).not.toBeUndefined();
     })
+
+    test('R-2: Get all user data in a list', () => {
+        const user = UserService.create(1, "AndresSantiag08", "Andrés");
+        const userInfoList = UserService.getInfo(user);
+
+        expect(userInfoList[0]).toBe(1);
+        expect(userInfoList[1]).toBe("AndresSantiag08");
+        expect(userInfoList[2]).toBe("Andrés");
+        expect(userInfoList[3]).toBe("Sin bio");
+    })
 })
