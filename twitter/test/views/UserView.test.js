7cc73794cc4850ccdb40d88f31cfa3ca7f5df003
AndreSantiago08 <harper_hska_5@hotmail.com>
2022-04-25 16:12:32 -0500

    Pasando requerimiento 2, sprint 3

--- twitter/test/views/UserView.test.js
+++ twitter/test/views/UserView.test.js
@@ -1,10 +1,17 @@
 const UserView = require('./../../app/views/UserView');
 
 describe("Test for UserView", () => {
-    test("S-3,R-1: Return an error object when try to create a new user with an null playload", () => {
+    test("S-3, R-1: Return an error object when try to create a new user with an null playload", () => {
         const payload = null;
         const result = UserView.createUser(payload);
 
         expect(result.error).toMatch(/payload no existe/);
+    })
+
+    test("S-3, R-2: Return an error object when try to create a new user with a payload with invalid properties", () => {
+        const payload = {userName: null, name: "Andres", id: 2};
+        const result = UserView.createUser(payload);
+
+        expect(result.error).toMatch(/necesitan tener un valor válido/);
     })
 })
