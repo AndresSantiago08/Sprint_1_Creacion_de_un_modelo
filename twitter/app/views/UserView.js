e5c909b40ea17bd9880db1c6147010ebe83efed3
AndreSantiago08 <harper_hska_5@hotmail.com>
2022-04-26 21:35:25 -0500

    Pasando requerimiento 4, sprint 3

--- twitter/app/views/UserView.js
+++ twitter/app/views/UserView.js
@@ -8,18 +8,14 @@ class UserView{
             const user = {id: "", userName: "", name: "", error: 'payload no existe'};
             return user
         }
-        else if (payload.userName == null || payload.name == null || payload.id == null){
-            const user = {id: "", userName: "", name: "", error: "necesitan tener un valor válido"};
-            return user
-        }
-        else if (payload.userName !== 'string' || payload.name !== 'string' || payload.userName === "" || 
-        payload.name === "" || payload.id === ""){
+        else if (payload.userName === "" || payload.name === "" || payload.id === "" || 
+        payload.userName == null || payload.name == null || payload.id == null){
             const user = {id: "", userName: "", name: "", error: "necesitan tener un valor válido"};
             return user
         }
         else{
             const userS = UserService.create(payload.id, payload.userName, payload.name);
-            const user = {id: userS.id, userName: "userS.userName", name: userS.name, error: ""}
+            const user = {id: userS.id, userName: userS.userName, name: userS.name, error: ""}
             return user
         }
     }
